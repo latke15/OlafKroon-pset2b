b16da93b6800f1acf35ebd4efbee3541f70f9cca
Olaf Kroon <Olaf@wcw-stud-145-109-8-217.wireless.uva.nl>
2016-11-18 14:07:34 +0100

    TODO: saving data when the app is closed.

---
+++ SecondViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SecondViewController.swift
+//  movielist2
+//
+//  Created by Olaf Kroon on 18/11/16.
+//  Copyright © 2016 Olaf Kroon. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    
+    var newData = String()
+
+    
+    @IBOutlet weak var plot: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("DATA IN SECOND VIEW CONTROLLER")
+        print(newData)
+        plot.text = newData
+        
+       
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- movielist2/ViewController.swift
+++ movielist2/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    let defaults = UserDefaults.standard
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchMovie: UITextField!
     @IBOutlet weak var enterMovie: UIButton!
@@ -19,22 +21,54 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     var data = [String: String]()
     var showRating = [String: String]()
     var showImage = [String: String]()
+    var showPlot = [String: String]()
+    var checkTitle = String()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    //If cell is pressed. Segue data to a next view.
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        checkTitle = titles[indexPath.row]
+        print(checkTitle)
+        self.performSegue(withIdentifier: "nextView", sender: nil)
+        
+    }
+    
+    // Make the rows editable.
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool
+    {
+        return true
+    }
+    
+    // Enable the user to delete rows. 
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == .delete
+        {
+            titles.remove(at: indexPath.row)
+            self.tableView.reloadData()
+        }
+    }
+    
+    // A segue function that is called when a row is pressed.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination: SecondViewController = segue.destination as! SecondViewController
+        destination.newData = self.showPlot[checkTitle]!
     }
 
+    // A function that returns the number of rows in the tableview.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
         return titles.count
     }
     
+    //  Updata the rows in tableview.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MovieCell
         
@@ -44,46 +78,43 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             cell.rating.text = rating
         }
         
+    
+    
+
+    // Functions to download an image with source: http://stackoverflow.com/questions/24231680/loading-downloading-image-from-url-on-swift
         
-        // Functions to download an image with source: http://stackoverflow.com/questions/24231680/loading-downloading-image-from-url-on-swift
-        func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
+    func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
             URLSession.shared.dataTask(with: url) {
                 (data, response, error) in
                 completion(data, response, error)
                 }.resume()
-        }
+    }
         
-        func downloadImage(url: URL) {
-            print("Download Started")
-            getDataFromUrl(url: url) { (data, response, error)  in
-                guard let data = data, error == nil else { return }
-                print(response?.suggestedFilename ?? url.lastPathComponent)
-                print("Download Finished")
-                DispatchQueue.main.async() { () -> Void in
-                    cell.poster.image = UIImage(data: data)
-                }
+    func downloadImage(url: URL) {
+        getDataFromUrl(url: url) { (data, response, error)  in
+            guard let data = data, error == nil else { return }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            print("Download Finished")
+            DispatchQueue.main.async() { () -> Void in
+                cell.poster.image = UIImage(data: data)
             }
         }
+    }
         
         // Put an image in the image view of every cell
         if let image = showImage[titles[indexPath.row]] {
-            print("Begin of code")
             if let checkedUrl = URL(string: image) {
                 cell.poster.contentMode = .scaleAspectFit
                 downloadImage(url: checkedUrl)
             }
-            print("End of code. The image will continue downloading in the background and it will be loaded when it ends.")
-            
-            
+           print("End of code. The image will continue downloading in the background and it will be loaded when it ends.")
         }
-
         
-    
-        
+        // Return the content of the cell.
         return cell
     }
     
-    
+    // If the button is clicked get a json from the title the user has given.
     @IBAction func getJson(_ sender: Any) {
         if enterMovie.isTouchInside {
             var searchRequest = String()
@@ -92,6 +123,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
             
             // Get a json from an URL source: http://stackoverflow.com/questions/38292793/http-requests-in-swift-3
+            
             let url = URL(string: searchRequest)
             let task = URLSession.shared.dataTask(with: url!) { data, response, error in
                 guard error == nil else {
@@ -109,16 +141,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 
                 // Parse the data into a json
                 let json = try! JSONSerialization.jsonObject(with: data, options: [])
-//                print("JSON)")
-//                print(json)
-                self.data = json as! [String : String]
-//                print(self.data)
-//                print("title")
-//                print(self.data["Title"]!)
-//                print("LINK")
-//                print(self.data["Poster"]!)
                 
-                //HIER NOG IF LET
+                // Put the json into a dictionary
+                self.data = json as! [String : String]
+
                 
                 //Add an item to titles array
                 if self.data["Title"] != nil {
@@ -129,27 +155,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 
                 //add a image url to the showImage dictionary with Title as key
                 self.showImage[self.data["Title"]!] = self.data["Poster"]
+                    
+                self.showPlot[self.data["Title"]!] = self.data["Plot"]
+                    
+                self.tableView.reloadData()
                 
-//                print("THESE ARE THE TITLES: ", self.titles)
-//                
-//                print("SHOWRATING: ", self.showRating)
                 }
-
-                
             }
-            
-            task.resume()
-            self.loadView()
-            
-            
+        task.resume()
+        self.loadView()
         }
-        
-
-        }
-    
-    
-    
     }
+}
 
 
 
